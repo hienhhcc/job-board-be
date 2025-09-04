5d515af90435d0eadfea8b0f73ac9194d42b99c4
hienhhcc <thachdau16t@gmail.com>
2025-09-04 18:11:45 +0700

    remove unnessesary files

--- drizzle/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from 'drizzle/schema';
-
-const db = drizzle(
-  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  { schema },
-);
-
-export default db;
--- features/users/db/users.ts
+++
@@ -1,6 +0,0 @@
-import db from 'drizzle/db';
-import { UserTable } from 'drizzle/schema';
-
-export async function insertUser(user: typeof UserTable.$inferInsert) {
-  await db.insert(UserTable).values(user).onConflictDoNothing();
-}
